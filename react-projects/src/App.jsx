5badc7220d9992d7c7074aef0e17e1a8470d2ba1
Felix Miranda <feedmite@hotmail.com>
2021-12-08 14:58:41 -0500

    reviews added

--- react-projects/src/App.jsx
+++ react-projects/src/App.jsx
@@ -1,41 +1,14 @@
 import { useState, useEffect } from "react";
 import "./App.css";
+import ProjectReview from "./components/reviews/ProjectReview";
 import Loading from "./components/tours/Loading";
 import Tours from "./components/tours/Tours";
-// import List from "./components/List";
-// import data from "./data";
 
 function App() {
-  // const [people, setPeople] = useState(data);
-  // console.log(people);
-  const url = "https://course-api.com/react-tours-project";
-  const [isLoading, setIsLoading] = useState(true);
-  const [tour, setTour] = useState([]);
-  const fetchTours = async () => {
-    try {
-      const response = await fetch(url);
-      const tours = await response.json();
-      console.log(tours);
-      setIsLoading(false);
-      setTour(tours);
-    } catch (err) {
-      setIsLoading(false);
-      console.log(err);
-    }
-  };
-  const removeTour = (id) => {
-    const newTour = tour.filter((tur) => tur.id != id);
-    setTour(newTour);
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
   return (
-    <main>
-      {isLoading ? <Loading /> : <Tours tours={tour} removeTour={removeTour} />}
-      {tour.length === 0 && <button onClick={fetchTours}>Refresh</button>}
-    </main>
+    <div>
+      <ProjectReview />
+    </div>
   );
 }
 
---
+++ react-projects/src/components/reviews/Review.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import "./review.css";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Review = () => {
+  const [index, setIndex] = useState(0);
+  console.log(people);
+  const { job, name, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4>{name}</h4>
+      <p className="job">{job}</p>
+      <p className="text">{text}</p>
+      <div className="btn-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">Surprise me</button>
+    </article>
+  );
+};
+
+export default Review;
